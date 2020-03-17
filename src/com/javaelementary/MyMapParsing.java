d58d25c3242685efef53db58e17c4b1d237bce20
Mariia Pasichna <pasichnayam@gmail.com>
2020-03-17 08:48:07 +0200

    correct

--- src/com/javaelementary/MyMapParsing.java
+++ src/com/javaelementary/MyMapParsing.java
@@ -8,30 +8,44 @@ import java.util.Map;
 public class MyMapParsing {
     public static void parse(Map<String, String> map, Student student) {
         List<Map.Entry<String, String>> list = new ArrayList<>(map.entrySet());
+        List<Map.Entry<String, String>> listForRecord = new ArrayList<>();
+        Field[] fields = student.getClass().getDeclaredFields();
+        for (int i = 0; i < list.size(); i++) {
+            for (Field field : fields) {
+                if (field.getName().equals(list.get(i).getKey())) {
+                    listForRecord.add(list.get(i));
+                }
+            }
+        }
         try {
-            for (int i = 0; i < list.size(); i++) {
-                Field field = student.getClass().getDeclaredField(list.get(i).getKey());
+            for (int i = 0; i < listForRecord.size(); i++) {
+                Field field = student.getClass().getDeclaredField(listForRecord.get(i).getKey());
                 Class fieldType = field.getType();
-                //if (field.getName().equals(list.get(i).getKey())){
                 field.setAccessible(true);
                 switch (fieldType.getName()) {
                     case "int":
-                        field.set(student, Integer.parseInt(list.get(i).getValue()));
+                        field.set(student, Integer.parseInt(listForRecord.get(i).getValue()));
                         break;
                     case "double":
-                        field.set(student, Double.parseDouble(list.get(i).getValue()));
+                        field.set(student, Double.parseDouble(listForRecord.get(i).getValue()));
+                        break;
+                    case "float":
+                        field.set(student, Float.parseFloat(listForRecord.get(i).getValue()));
                         break;
                     case "long":
-                        field.set(student, Long.parseLong(list.get(i).getValue()));
+                        field.set(student, Long.parseLong(listForRecord.get(i).getValue()));
                         break;
                     case "byte":
-                        field.set(student, Byte.parseByte(list.get(i).getValue()));
+                        field.set(student, Byte.parseByte(listForRecord.get(i).getValue()));
                         break;
                     case "boolean":
-                        field.set(student, Boolean.parseBoolean(list.get(i).getValue()));
+                        field.set(student, Boolean.parseBoolean(listForRecord.get(i).getValue()));
+                        break;
+                    case "java.lang.String":
+                        field.set(student, listForRecord.get(i).getValue());
                         break;
                     default:
-                        field.set(student, list.get(i).getValue());
+                        System.out.println("Unexpected type: " + fieldType.getName());
                         break;
                 }
                 field.setAccessible(false);
--- src/com/javaelementary/Task2.java
+++ src/com/javaelementary/Task2.java
@@ -8,6 +8,8 @@ public class Task2 {
         Map<String, String> map = new LinkedHashMap<>();
         map.put("name", "Alex");
         map.put("age", "22");
+        map.put("group", "java");
+        map.put("dateOfStart", "22.01.2020");
         Student student = new Student();
         MyMapParsing.parse(map, student);
         System.out.println(student);
